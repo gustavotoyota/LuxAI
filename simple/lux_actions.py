Fixed considered units and sample generation

simple/lux_actions.py
@@ -136,14 +136,13 @@ considered_units_map: List[List[Unit]]) -> List[List[Action]]:
 
 
 
-def get_env_actions(team_actions: Tuple[list, list], game: Game) -> Tuple[List[Action], List[Action]]:
+def get_env_actions(game: Game, team_actions: Tuple[list, list], 
+considered_units_map: List[List[Unit]]) -> Tuple[List[Action], List[Action]]:
   env_actions = []
 
 
   for team in range(2):
-    team_considered_units_map = get_team_considered_units_map(game, team)
-    
-    env_actions += get_team_env_actions(team_actions[team], game, team, team_considered_units_map)
+    env_actions += get_team_env_actions(team_actions[team], game, team, considered_units_map)
 
 
   return env_actions
@@ -168,3 +167,16 @@ def get_team_action_probs(team_actions: List[List[tuple]], cell_action_probs):
 
 
   return team_action_probs / np.sum(team_action_probs)
+
+
+
+
+def get_team_action_policy(game: Game, team_action: List[tuple]):
+  team_action_policy = np.zeros((CELL_ACTION_COUNT, game.configs['width'], game.configs['height']))
+
+
+  for cell_action in team_action:
+    team_action_policy[cell_action] = 1.0
+
+
+  return team_action_policy

simple/lux_inputs.py
@@ -115,7 +115,7 @@ INPUT_COUNT = len(input_list)
 
 
 
-def get_team_observation(game: Game, team: int, team_considered_units_map: List[List[Unit]]):
+def get_team_observation(game: Game, team: int, considered_units_map: List[List[Unit]]):
   team_observation = np.zeros((INPUT_COUNT, game.map.configs['height'], game.map.configs['width']))
 
 
@@ -193,7 +193,7 @@ def get_team_observation(game: Game, team: int, team_considered_units_map: List[
       
       # Unit
 
-      considered_unit: Unit = team_considered_units_map[y][x]
+      considered_unit: Unit = considered_units_map[y][x]
 
       if considered_unit:
         if considered_unit.is_worker():

simple/lux_selfplay.py
@@ -4,6 +4,9 @@ from luxai2021.game.game import Game
 
 import torch
 
+import time
+import pickle
+
 
 
 from lux_actions import *
@@ -22,19 +25,37 @@ def selfplay(map_size: int, model: LuxModel):
   
   mcts = MCTS(model)
 
+  samples = []
+
   while not game.match_over():
     team_actions = mcts.run(game)
+      
+    target_value = mcts.root.cumul_value / mcts.root.num_visits
+    
+    considered_units_map = get_considered_units_map(game)
 
-    env_actions = get_env_actions(team_actions, game)
+    for team in range(2):
+      team_observation = get_team_observation(game, team, considered_units_map)
+
+      target_policy = get_team_action_policy(game, team_actions[team])
+
+      samples.append((team_observation, target_policy, target_value * (-team * 2 + 1)))
+
+    env_actions = get_env_actions(game, team_actions, considered_units_map)
 
     game.run_turn_with_actions(env_actions)
 
-    print(game.state['turn'])
+    print('Turn:', game.state['turn'])
+    print(game.map.get_map_string())
 
   print('Winner: ' + str(game.get_winning_team()))
+
+  with open('samples/Samples ' + time.strftime('%Y.%m.%d - %H.%M.%S') + '.pickle', 'wb') as f:
+    pickle.dump(samples, f)
 
 
 if torch.cuda.is_available():
   torch.set_default_tensor_type(torch.cuda.FloatTensor)
   
-selfplay(12, LuxModel(12, 12))
+while True:
+  selfplay(12, LuxModel(12, 12))