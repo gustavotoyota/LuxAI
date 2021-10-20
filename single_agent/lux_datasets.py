02e287f4b21334f5f7e5a86c396f75f40d479660
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-10-20 13:21:23 -0400

    Implemented HDF5 dataset

--- single_agent/lux_datasets.py
+++ single_agent/lux_datasets.py
@@ -1,46 +1,31 @@
-import os
 import torch.utils.data
 import torch.utils.data.dataset
 
 
 
 
-import lux_utils
+import h5py
 
 
 
 
-class LuxEpisodeDataset(torch.utils.data.dataset.Dataset):
+class HDF5Dataset(torch.utils.data.dataset.Dataset):
   def __init__(self, file_path):
-    self.file_path = file_path
+    self.hdf5_file = h5py.File(file_path, 'r')
 
-
+    self.hdf5_inputs = self.hdf5_file['inputs']
+    self.hdf5_actions = self.hdf5_file['actions']
+    self.hdf5_values = self.hdf5_file['values']
 
 
   def __len__(self):
-    samples = lux_utils.load_lz4_pickle(self.file_path)
-
-    return samples[0].shape[0]
-
+    return self.hdf5_inputs.shape[0]
 
 
 
   def __getitem__(self, index):
-    samples = lux_utils.load_lz4_pickle(self.file_path)
-
-    return samples[0][index], samples[1][index], samples[2][index]
-
-
-
-
-def get_dataset(dir_path, map_size):
-  episode_datasets = []
-
-  dir_path = f'{dir_path}/{map_size}'
-
-  for file_name in os.listdir(dir_path):
-    file_path = f'{dir_path}/{file_name}'
-    
-    episode_datasets.append(LuxEpisodeDataset(file_path))
-
-  return torch.utils.data.ConcatDataset(episode_datasets)
+    return (
+      self.hdf5_inputs[index],
+      self.hdf5_actions[index],
+      self.hdf5_values[index]
+    )
